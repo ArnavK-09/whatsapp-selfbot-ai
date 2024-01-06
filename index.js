199c159914fc853d3be8058b3cb1cad7575d0160
Arnav K <arnavkaushal09@gmail.com>
2024-01-06 17:40:45 +0000

    /image cmd

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 // imports 
-const { Client } = require('whatsapp-web.js');
+const { Client, MessageMedia } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const { consola } = require("consola");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+const OpenAI = require("openai");
 
 // prompt 
 const prompt = `You are "Arnav", Arnav is a teen boy who's birthday is 9th March 2008! You have to act like Arnav, reply friendly, humble and humour manner. Don't try to swear or disrespect anybody! Just reply to user as Arnav, also include emojis in your replies. Reply in precise and Crisp way! You are student and loves humanity! Just reply casual conversations in 2-4 lines and you can elaborate knowledge questions answers and reply only answer not extra things! You live in Panchkula, Haryana, India! Respond accurately, Just send me answer for the "User" conversation don't include "Arnav:"!.`;
 
 // new ai client
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_KEY);
+const ai2 = new OpenAI({
+    apiKey: process.env.AI_KEY,
+    baseURL: "https://api.naga.ac/v1"
+});
 const ai = genAI.getGenerativeModel({
     model: "gemini-pro"
 })
-consola.info(`Using API Key: ${process.env.GEMINI_KEY}`);
+consola.info(`Using GEMINI API Key: ${process.env.GEMINI_KEY}`);
+consola.info(`Using SECONDARY API Key: ${process.env.AI_KEY}\n`);
 
 // new whatsapp client 
 const client = new Client();
@@ -31,16 +37,17 @@ client.on('ready', () => {
 });
 
 // Handles Commands 
-// client.on("message", async (msg) => handler(msg))
-// Handles Self Commands 
 client.on("message_create", async (msg) => handler(msg))
 
 /**
  * ALL Commands Handler
  */
 async function handler(msg) {
-    if (msg.body.startsWith("/ask")) {
+    if (msg.body.toLowerCase().startsWith("/ask")) {
         await cmdAsk(msg)
+    }
+    if (msg.body.toLowerCase().startsWith("/image")) {
+        await cmdImage(msg)
     }
 }
 
@@ -53,9 +60,34 @@ async function cmdAsk(msg) {
         const result = await ai.generateContent(`${prompt}\nUser: ${content}`);
         const response = result.response;
         const text = response.text();
-        consola.box(`User: ${msg.from.toString().split("@")[0]}\nMessage: ${content}\nReply: ${text}`);
+        consola.box(`User: +${msg.from.toString().split("@")[0]}\nMessage: ${content}\nReply: ${text}`);
         msg.reply(text)
     } catch (e) {
+        msg.reply(`🛑 *ERROR*: ${e.message}`)
+        consola.error(e.message)
+    }
+}
+
+/**
+ * Handles /ask command
+*/
+async function cmdImage(msg) {
+    const content = msg.body.replace("/image", "") ?? "";
+    try {
+        const response = await ai2.images.generate({
+            model: "sdxl",
+            prompt: content ?? "Image",
+            n: 1,
+            size: "1024x1024",
+        });
+        console.log(response)
+        const image_url = response.data[0].url;
+        const media = await MessageMedia.fromUrl(image_url);
+        const chat = await msg.getChat()
+        chat.sendMessage(media, { caption: `Creator: +${msg.from.toString().split("@")[0]}\nPrompt: ${content}` });
+        consola.box(`Creator: ${msg.from.toString().split("@")[0]}\nPrompt: ${content}\nImage: ${image_url}`);
+    } catch (e) {
+        msg.reply(`🛑 *ERROR*: ${e.message}`)
         consola.error(e.message)
     }
 }
@@ -68,5 +100,5 @@ process.on('unhandledRejection', e => {
 });
 
 // init client 
-//client.initialize();
+// client.initialize();
 module.exports = client;

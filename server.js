b7e228423a0369f4a346b6754c140620b6c5aab0
Arnav K <arnavkaushal09@gmail.com>
2024-01-06 17:42:43 +0000

    new msg

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const app = express();
 const port = 3001; // You can use any available port
 
 // Define a route
-app.get('/', (req, res) => {
-  res.send('Hello, Express!');
+app.get('/', (_, res) => {
+  res.send('Hello From Whatsapp Bot!');
 });
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
-client.initialize()
+  client.initialize()
 });
